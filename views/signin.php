85937996eced6195e16b5dc350acbd044e51e629
Threesixty <michael.convergence@gmail.com>
2021-05-11 23:22:16 +0200

    Style fix

--- views/signin.php
+++ views/signin.php
@@ -45,7 +45,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		<div class="d-flex flex-column flex-root">
 			<!--begin::Login-->
 			<div class="login login-5 d-flex flex-row-fluid login-<?= $formAction ?>-on" id="kt_login">
-				<div class="d-flex flex-center bgi-size-cover bgi-no-repeat flex-row-fluid bg-mobile" style="background-image: url(assets/media/bg/bg-2.jpg);">
+				<div class="d-flex flex-center bgi-size-cover bgi-no-repeat flex-row-fluid bg-mobile" style="background-image: url(assets/media/bg/bg-3.jpg);">
 					<div class="login-form text-center text-white p-7 position-relative overflow-hidden">
 						<!--begin::Login Sign in form-->
 						<div class="login-signin">
